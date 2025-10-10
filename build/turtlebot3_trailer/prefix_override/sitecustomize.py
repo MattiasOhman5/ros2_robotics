f810fca7419a529a8d6c14c8709ad683dde5dcad
Rasmus Tengstedt <114366904+Seglats@users.noreply.github.com>
2025-10-10 16:18:34 +0200

    Delete build directory

--- build/turtlebot3_trailer/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/home/ubuntuuser/ros2_ws/.venv':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ubuntuuser/ros2_ws/install/turtlebot3_trailer'
