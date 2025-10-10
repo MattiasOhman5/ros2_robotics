f0002397a12fb3851a9ce3a472463b88840e50aa
Rasmus Tengstedt <114366904+Seglats@users.noreply.github.com>
2025-10-10 16:19:28 +0200

    Delete src/turtlebot3_trailer directory

--- src/turtlebot3_trailer/build/turtlebot3_trailer/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ubuntuuser/ros2_ws/src/turtlebot3_trailer/install/turtlebot3_trailer'
